76c8484a273790979f615d4aa9dbf0fd64673926
Aiji Uejima <aiji42@gmail.com>
2020-05-06 17:22:45 +0900

    setup eslint && implement getConversation from Dynamodb

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,5 @@
 FROM node:12.16.3-alpine
 
-ARG AWS_ACCESS_KEY_ID
-ARG AWS_SECRET_ACCESS_KEY
-
 RUN apk add --no-cache \
   python \
   py-pip \
@@ -11,8 +8,6 @@ RUN apk add --no-cache \
 RUN pip install --no-cache-dir --upgrade pip awscli
 
 RUN yarn global add serverless --prefix /usr/local
-
-RUN sls config credentials --provider aws --key $AWS_ACCESS_KEY_ID --secret $AWS_SECRET_ACCESS_KEY
 
 RUN mkdir -p /app
 WORKDIR /app
--- handler.ts
+++ handler.ts
@@ -1,12 +1,23 @@
 import { APIGatewayProxyHandler } from 'aws-lambda';
 import 'source-map-support/register';
+import { DynamoDB } from 'aws-sdk';
 
-export const hello: APIGatewayProxyHandler = async (event, _context) => {
+const dynamodb = new DynamoDB.DocumentClient({
+  region: process.env.AWS_DEFAULT_REGION,
+  endpoint: 'http://dynamodb:8000',
+  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
+});
+
+export const getConversation: APIGatewayProxyHandler = async (event) => {
+  const { pathParameters: { serviceCode } } = event;
+  const result = await dynamodb.get({ TableName: 'botui-conversations', Key: { serviceCode } }).promise();
+
+  if (!Object.keys(result).length) return { statusCode: 404, body: JSON.stringify({ message: `not found serviceCode: ${serviceCode}` }) };
+
+  const { Item: { conversations } } = result;
   return {
     statusCode: 200,
-    body: JSON.stringify({
-      message: 'Go Serverless Webpack (Typescript) v1.0! Your function executed successfully!',
-      input: event,
-    }, null, 2),
+    body: JSON.stringify(conversations, null, 2),
   };
-}
+};
